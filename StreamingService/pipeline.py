bda90fb4af525faede0b609db6529041266fec8f
pengnam <seanngpengnam@gmail.com>
2018-08-07 15:15:09 +0800

    feat(pipeline): add write function

--- StreamingService/pipeline.py
+++ StreamingService/pipeline.py
@@ -12,3 +12,23 @@ class DataPipeline:
         self.schema = avro.schema.Parse(open(schema_path).read())
         self.keys = [field.name for field in self.schema.fields]
 
+
+
+    def write(self, data):
+        print(data)
+        store_data = {}
+        for key in self.keys:
+            if key in data:
+                store_data[key] = data[key]
+            else:
+                store_data[key] = None
+        writer = DatumWriter(self.schema)
+        bytes_writer = io.BytesIO()
+        encoder=avro.io.BinaryEncoder(bytes_writer)
+
+        writer.write(store_data, encoder)
+        raw_bytes = bytes_writer.getvalue()
+        print(raw_bytes)
+
+
+
