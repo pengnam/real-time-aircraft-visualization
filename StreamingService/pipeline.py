571e491d6fbbadf29b571778626e5980a04f8d4b
pengnam <seanngpengnam@gmail.com>
2018-08-07 16:42:39 +0800

    feat(pipeline): split pipeline into pipeline base and data pipeline producer

--- StreamingService/pipeline.py
+++ StreamingService/pipeline.py
@@ -5,30 +5,33 @@ from avro.io import DatumWriter
 
 
 class DataPipeline:
-    def __init__(self, topic, schema_path, host='localhost:9092'):
-        #self.producer = KafkaProducer(bootstrap_servers=host)
+    def __init__(self, topic, schema_path):
         self.topic = topic
-
         self.schema = avro.schema.Parse(open(schema_path).read())
         self.keys = [field.name for field in self.schema.fields]
 
 
+class DataPipelineProducer(DataPipeline):
+    def __init__(self, host='localhost:9092', *args, **kwargs):
+        self.producer = self.KafkaProducer(boostrap_server=host)
+        super().__init__(*args, **kwargs)
 
     def write(self, data):
-        print(data)
+        #Parsing data to select only keys in schema
         store_data = {}
         for key in self.keys:
             if key in data:
                 store_data[key] = data[key]
             else:
                 store_data[key] = None
+
+        #Serialize data using AVRO
         writer = DatumWriter(self.schema)
         bytes_writer = io.BytesIO()
         encoder=avro.io.BinaryEncoder(bytes_writer)
 
         writer.write(store_data, encoder)
         raw_bytes = bytes_writer.getvalue()
-        print(raw_bytes)
 
 
 
