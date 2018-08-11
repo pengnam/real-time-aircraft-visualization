84b0a7e2487a8bfda632d9d90f5223c7f54e39ce
pengnam <seanngpengnam@gmail.com>
2018-08-11 23:27:06 +0800

    feat(ClientSide): add basic app.js

---
+++ WebService/ClientSide/app.js
@@ -0,0 +1,99 @@
+/* global window */
+import React, {Component} from 'react';
+import MapGL from 'react-map-gl';
+import DeckGLOverlay from './deckgl-overlay';
+import {LayerControls, SCATTERPLOT_CONTROLS} from './layer-controls';
+import {tooltipStyle} from './style';
+import taxiData from '../../../data/taxi';
+
+const MAPBOX_STYLE = 'mapbox://styles/mapbox/dark-v9';
+// Set your mapbox token here
+const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+const MY_DATA = [];
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        longitude: -74,
+        latitude: 40.7,
+        zoom: 11,
+        maxZoom: 16
+      },
+      points: [],
+      settings: Object.keys(SCATTERPLOT_CONTROLS).reduce((accu, key) => ({
+        ...accu,
+        [key]: SCATTERPLOT_CONTROLS[key].value
+      }), {}),
+      // hoverInfo
+      x: 0,
+      y: 0,
+      hoveredObject: null,
+      status: 'LOADING'
+    };
+    this._resize = this._resize.bind(this);
+		//Starts websocket (hopefully only once)
+		console.log("Starting websocket")
+    this._getData = this._getData.bind(this);
+    this._processData = this._processData.bind(this);
+    this._getData();
+  }
+
+  _onHover({x, y, object}) {
+    this.setState({x, y, hoveredObject: object});
+  }
+
+  _onViewportChange(viewport) {
+    this.setState({
+      viewport: {...this.state.viewport, ...viewport}
+    });
+  }
+
+  _resize() {
+    this._onViewportChange({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  }
+
+  _updateLayerSettings(settings) {
+    this.setState({settings});
+  }
+
+  render() {
+    console.log("This is rendering so this is great");
+    return (
+      <div>
+        {this.state.hoveredObject &&
+          <div style={{
+            ...tooltipStyle,
+            transform: `translate(${this.state.x}px, ${this.state.y}px)`
+          }}>
+            <div>{JSON.stringify(this.state.hoveredObject)}</div>
+          </div>}
+        <LayerControls
+          settings={this.state.settings}
+          propTypes={SCATTERPLOT_CONTROLS}
+          onChange={settings => this._updateLayerSettings(settings)}/>
+        <MapGL
+          {...this.state.viewport}
+          mapStyle={MAPBOX_STYLE}
+          onViewportChange={viewport => this._onViewportChange(viewport)}
+          mapboxApiAccessToken={MAPBOX_TOKEN}>
+          <DeckGLOverlay
+            viewport={this.state.viewport}
+            data={this.state.points}
+            onHover={hover => this._onHover(hover)}
+            {...this.state.settings}
+          />
+        </MapGL>
+      </div>
+    );
+  }
+}
+
+
